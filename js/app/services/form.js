b94ee3842c6dee404d26371cfbea9ee15b1535fb
sergey rudenko <rudenko.sergey92@gmail.com>
2014-01-21 09:58:07 +0200

    url back to local

--- js/app/services/form.js
+++ js/app/services/form.js
@@ -2,8 +2,8 @@
 
 app.service('FormService', function FormService($http) {
     //for local testing set local url
-    var url = 'http://hfyljv.uni.me/task-form/api';
-    //var url = './data/data.json';
+    //var url = 'http://hfyljv.uni.me/task-form/api';
+    var url = './data/data.json';
 
     return {
         form: function() {
