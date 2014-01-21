d6f80f34d31a41127ecdfbb21c294fe420e58d40
Sergey Rudenko <rudenko.sergey92@gmail.com>
2014-01-21 16:57:01 +0200

    Update form.js

--- js/app/services/form.js
+++ js/app/services/form.js
@@ -8,14 +8,14 @@ app.service('formService', function ($http) {
     return {
         form: function() {
             return $http.jsonp(url).success(function(response) {
-                if(response.data.status === 'Success') {
-                    response.data.errorStatus = false;
-                    return response.data;
+                if(response.status === 'Success') {
+                    response.errorStatus = false;
+                    return response;
                 }
                 else {
-                    var resp = {'Error':'Invalid data from server', errorStatus:true};
                     console.log('Invalid data from server');
-                    return resp;
+                    response.errorStatus = true;
+                    return response;
                 }
             });
         }
